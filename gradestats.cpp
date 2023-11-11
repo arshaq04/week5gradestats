d5192913c4ad5c5f241b343e44339a9959b4d7cd
Arshaq Ahmed <arshahm2004@gmail.com>
2023-11-11 22:47:24 +0000

    initial commit

---
+++ gradestats.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    
+     vector<int> grades;
+     int x;
+     cout << "Enter grade (or -1 to end): " << endl;
+
+     for (int i = 0; i < grades.size(); i++) {
+        while ()
+     }
+
+     cout << grades[i] << endl;
+
+    return 0;
+}
