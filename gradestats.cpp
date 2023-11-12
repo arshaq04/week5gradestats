a3ee67d32055f5e9dea5984d65e60d259fbce1d5
Arshaq Ahmed <arshahm2004@gmail.com>
2023-11-12 22:50:06 +0000

    added condition and definitions

--- gradestats.cpp
+++ gradestats.cpp
@@ -4,16 +4,22 @@
 using namespace std;
 
 int main() {
-    
      vector<int> grades;
-     int x;
-     cout << "Enter grade (or -1 to end): " << endl;
-
-     for (int i = 0; i < grades.size(); i++) {
-        while ()
+     for (int i = 0; i <= 100; i++) { //assume there is a max of 100 grades
+         int x; 
+         cout << "Enter grade (or -1 to end): ";
+         cin >> x; // read int x intered
+         
+         if (x == -1) {  // if -1 typed in then exit loop
+             break;
+         }
+         grades.push_back(x); // push one element into the array
      }
-
-     cout << grades[i] << endl;
-
+     int sum = 0;
+     for (int i : grades) {
+         sum += i; // int sum to add up all integer elements in the array
+     }
+     cout << "Average: " << (sum / grades.size()) << endl;
+     // to calculate average we can do sum divided by how elements are in the array
     return 0;
 }
